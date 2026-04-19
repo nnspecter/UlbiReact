39d9f263af3e2c26f307dc7dab4d051fa50152d7
nnspecter <nnspecter@gmail.com>
2026-04-19 11:00:10 +0300

    feat: react-router + router-dom + добавление чанков чз lazy+suspense + esNext модульность

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import Counter from './components/Counter'
+import { Link, Route, Routes } from 'react-router-dom'
 import "./index.scss"
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import { Suspense } from 'react'
+
 
 const App = () => {
   return (
     <div className='app'>
-      App файлик  
-      <Counter/>
+      <Link to="/">Главная</Link>
+      <Link to="/about">О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+            <Route path={"/about"} element={<AboutPageAsync/>}></Route>
+            <Route path={"/"} element={<MainPageAsync/>}></Route>
+        </Routes>
+      </Suspense>
     </div>
   )
 }
