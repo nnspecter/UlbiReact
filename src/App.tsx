8201ae81a908470850357977073fe4544ddef07a
nnspecter <nnspecter@gmail.com>
2026-04-21 12:27:08 +0300

    feat: styles, themeContext, Provider, hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { Link, Route, Routes } from 'react-router-dom'
-import "./index.scss"
+import "./styles/index.scss"
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
 import { MainPageAsync } from './pages/MainPage/MainPage.async'
-import { Suspense } from 'react'
+import { Suspense, useContext, useState } from 'react'
+import { useTheme } from './theme/useTheme'
 
+export enum Theme {
+  LIGHT = 'light',
+  DARK = "dark"
+}
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme();
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>SetTheme</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
