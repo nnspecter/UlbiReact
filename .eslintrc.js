70ca5903a7c943bd41a9fc3955b8c11f62d24e9b
nnspecter <nnspecter@gmail.com>
2026-04-28 10:01:57 +0300

    feat: BundleAnalyzerPlugin для вебпака

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,6 +43,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
+        'max-len': ['error', { ignoreCommnts: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
